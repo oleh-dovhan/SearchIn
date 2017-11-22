66350897cee51458b1ca8575346d8400ae5db6b9
Oleh Dovhan <oleh.dovhan.731@outlook.com>
2017-11-22 23:22:04 +0200

    Added search component

--- SearchIn.Client/src/app/app.module.ts
+++ SearchIn.Client/src/app/app.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [SearchComponent]
 })
 export class AppModule { }
---
+++ SearchIn.Client/src/app/models/scan-state.ts
@@ -0,0 +1,6 @@
+export enum ScanState {
+    Downloading,
+    Found,
+    NotFound,
+    Error
+}
